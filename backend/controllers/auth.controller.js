a80e793f23d7dd901da86607521202e666999b3a
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-18 21:19:25 +0530

    Add authentication routes and controllers; configure server with nodemon

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signupUser = (req, res) => {
+  res.send("Sign Route");
+};
+export const loginUser = (req, res) => {
+  res.send("Login Route");
+};
+
+export const logoutUser = (req, res) => {
+  res.send("Logout Route");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
-const express = require("express")
+import express from "express"
+import dotenv from 'dotenv'
+import authRoutes from "./routes/auth.routes.js";
 
-const app = express()
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   //root route "http://localhost:5000/"
-  res.send("Server is running")
-})
+  res.send("Server is running");
+});
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000")
-})
+
+//auth route middleware
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
