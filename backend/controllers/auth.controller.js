b871b5a18b37f8812430ed54174094b96b06df50
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-18 22:35:59 +0530

    Implement user signup functionality with password hashing and validation; add MongoDB connection

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,54 @@
-export const signupUser = (req, res) => {
-  res.send("Sign Route");
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const signupUser = async (req, res) => {
+  try {
+    const { fullname, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        error: "Passwords do not match",
+      });
+    }
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({
+        error: "User already exists",
+      });
+    }
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatarapi.runflare.run/public/boy?usearname=${username}`;
+    const girlProfilePic = `https://avatarapi.runflare.run/public/girl?usearname=${username}`;
+
+    const newUser = new User({
+      fullname,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid user data",
+      });
+    }
+  } catch (error) {
+    console.log("Error signing up controller:", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
 export const loginUser = (req, res) => {
   res.send("Login Route");
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-import express from "express"
-import dotenv from 'dotenv'
-import authRoutes from "./routes/auth.routes.js";
+import express from "express";
+import dotenv from "dotenv";
 
-const app = express();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDb.js";
 
 dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  //root route "http://localhost:5000/"
-  res.send("Server is running");
-});
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
+// app.get("/", (req, res) => {
+//   root route "http://localhost:5000/"
+//   res.send("Server is running");
+// });
 
 //auth route middleware
-app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
