6291dd2ca137ce97977b22565f1c60d711eb85b9
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-19 19:59:01 +0530

    Refactor signup and login functionality; add error handling and user logout implementation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,12 +4,14 @@ import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
   try {
+    //accept credentials
     const { fullname, username, password, confirmPassword, gender } = req.body;
     if (password !== confirmPassword) {
       return res.status(400).json({
         error: "Passwords do not match",
       });
     }
+    //check if user already exists
     const user = await User.findOne({ username });
     if (user) {
       return res.status(400).json({
@@ -20,9 +22,11 @@ export const signupUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    // Create a new user pic
     const boyProfilePic = `https://avatarapi.runflare.run/public/boy?usearname=${username}`;
     const girlProfilePic = `https://avatarapi.runflare.run/public/girl?usearname=${username}`;
 
+    // Create a new user
     const newUser = new User({
       fullname,
       username,
@@ -31,6 +35,7 @@ export const signupUser = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
+    //save user
     if (newUser) {
       generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
@@ -52,10 +57,49 @@ export const signupUser = async (req, res) => {
     });
   }
 };
-export const loginUser = (req, res) => {
-  res.send("Login Route");
+export const loginUser = async (req, res) => {
+  try {
+    //check credentials
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || "",
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({
+        error: "Invalid username or password",
+      });
+    }
+    //generate token
+    generateTokenAndSetCookie(user._id, res);
+    //send response
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error login controller:", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
 
 export const logoutUser = (req, res) => {
-  res.send("Logout Route");
+  try {
+    //clear cookie
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.log("Error logout controller:", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
