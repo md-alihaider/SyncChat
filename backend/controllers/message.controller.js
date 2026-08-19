0330629deb0e97e24e2ab08b189303e2f5e257a0
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-19 22:33:26 +0530

    Completed Message Routes Api

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -30,9 +30,41 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
+    //SOCKET.IO FUCNCTIONALITY GOES HERE
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    //this is will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in send message controller:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    //accept payload
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    //check if conversation exists
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //not reference but actual object
+
+    if (!conversation) {
+      return res.status(404).json({ error: "Conversation not found" });
+    }
+    //send response
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in get message controller:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
