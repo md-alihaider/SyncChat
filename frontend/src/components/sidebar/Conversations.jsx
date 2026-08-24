c92e221fe3e381d1815c085c3fcd61b6dec0cb50
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-24 07:13:43 +0530

    feat: implement conversation fetching and random emoji assignment

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,22 @@
+import useGetConversation from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emoji";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversation();
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, index) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastidx={index === conversations.length - 1}
+        />
+      ))}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 };
