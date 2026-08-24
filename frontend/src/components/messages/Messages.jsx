6e03992d05a6181e7d83b0ee7c02d047c93b8148
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-24 09:14:52 +0530

    feat: enhance message rendering with smooth scroll to last message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,26 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+import { useEffect } from "react";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("messages: ", messages);
+  const lastMessageRef = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    },100)
+  }, [messages]);
   return (
     <div className="px-4 flex-1 overflow-auto">
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id}
+            ref={lastMessageRef}
+          >
+            <Message message={message} />
+          </div>
         ))}
       {loading &&
         [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
