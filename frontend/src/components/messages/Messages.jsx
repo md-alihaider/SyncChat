10bc34b7d9975257c8462896908207015f1a987b
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-24 09:08:21 +0530

    feat: implement message skeleton loading state and enhance message display logic

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,23 +1,25 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
-  const {messages,loading} = useGetMessages();
-  console.log("messages: ",messages);
+  const { messages, loading } = useGetMessages();
+  console.log("messages: ", messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-gray-400">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 };
