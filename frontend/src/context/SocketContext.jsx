939148c0fb2436ef3c38994e0a00f685ce0ac994
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-25 19:08:25 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://syncchat-mnrj.onrender.com", {
         query: {
           userId: authUser._id,
         },
